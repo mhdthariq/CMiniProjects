bcb4140a4c15bfa51bf16e153f3f9f57b7c7a9ad
Ren Hoshizora <mthariqaryaputra1@gmail.com>
2025-02-06 08:56:55 +0700

    add SimpleCalculator.c

---
+++ SimpleCalculator.c
@@ -0,0 +1,37 @@
+// C Program to implement a Simple Calculator
+// using if-else statements
+#include <float.h>
+#include <stdio.h>
+
+int main() {
+    char op;
+    double a, b, res;
+
+    // Read the operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    // Read the two numbers
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &a, &b);
+
+    // Perform the operation corresponding to the
+    //  given operator
+    if (op == '+')
+        res = a + b;
+    else if (op == '-')
+        res = a - b;
+    else if (op == '*')
+        res = a * b;
+    else if (op == '/')
+        res = a / b;
+    else {
+        printf("Error! Operator is not correct.\n");
+        res = -DBL_MAX;
+    }
+
+    if (res != -DBL_MAX)
+        printf("Result: %.2lf\n", res);
+
+    return 0;
+}
